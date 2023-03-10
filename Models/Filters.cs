8010603404ebfd0949b07ec4c29cc2815137af02
tima.kolesnik.2015@mail.ru <tima.kolesnik.2015@mail.ru>
2023-03-10 14:53:46 +0200

    Добавьте файлы проекта.

---
+++ Models/Filters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MembersSkolkovoParsing
+{
+    public class Filters
+    {
+        public string tags_type { get; set; }
+
+    }
+}
---
+++ Models/Root.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MembersSkolkovoParsing
+{
+    public class Root
+    {
+        public string key { get; set; }
+        public string sort { get; set; }
+        public int page { get; set; }
+        public Filters filters { get; set; }
+        public int limit { get; set; }
+    }
+}
